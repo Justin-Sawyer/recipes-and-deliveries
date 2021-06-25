6450b49e7fbbd152777be63829fcf4bbeb01ecf2
Justin-Sawyer <justin.sawyer@icloud.com>
2021-06-25 10:34:01 +0000

    Gluten free optioning added

--- products/models.py
+++ products/models.py
@@ -28,6 +28,9 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image_credit = models.CharField(max_length=254, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    has_gluten = models.BooleanField(default=False, null=True, blank=True)
+    gluten_free_option = models.BooleanField(default=False, null=True,
+                                             blank=True)
 
     def __str__(self):
         return self.name
