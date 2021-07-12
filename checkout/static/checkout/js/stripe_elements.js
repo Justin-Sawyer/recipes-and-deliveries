b832de2b885e78584045a376482b0a323b8fcebe
Justin-Sawyer <justin.sawyer@icloud.com>
2021-07-12 13:12:04 +0000

    passing information from form to stripe securely via the payment intent and recapturing it in the webhook

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -47,9 +47,42 @@ form.addEventListener('submit', function(ev) {
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(clientSecret, {
+    var saveInfo = Boolean($('#id_save_info').attr('checked'));
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save-info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+    $.post(url, postData).done(function() {
+      stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
+            billing_details: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                email: $.trim(form.email.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    state: $.trim(form.county.value),
+                }
+            }
+        },
+        shipping: {
+            name: $.trim(form.full_name.value),
+            phone: $.trim(form.phone_number.value),
+            address: {
+                line1: $.trim(form.street_address1.value),
+                line2: $.trim(form.street_address2.value),
+                city: $.trim(form.town_or_city.value),
+                country: $.trim(form.country.value),
+                postal_code: $.trim(form.postcode.value),
+                state: $.trim(form.county.value),
+            }
         }
     }).then(function(result) {
         if (result.error) {
@@ -70,5 +103,8 @@ form.addEventListener('submit', function(ev) {
                 form.submit();
             }
         }
-    });
+    });  
+    }).fail(function() {
+        location.reload();
+    })  
 });
