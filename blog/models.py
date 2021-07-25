ab21aa799911a208c6fd1d5acb873baf3e25359b
Justin-Sawyer <justin.sawyer@icloud.com>
2021-07-25 12:40:23 +0000

    another try to get date working... added to the model, migrated

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from datetime import datetime
-#from django.utils import timezone
+from datetime import datetime
+# from django.utils import timezone
 
 
 class Category(models.Model):
@@ -44,7 +44,7 @@ class Post(models.Model):
     image_credit = models.CharField(max_length=254, default="", blank=False)
     content = models.TextField()
     # date = models.DateTimeField(default=datetime.now, blank=True)
-    # date_posted = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
     # date_posted = models.DateTimeField(null=True, blank=True, default=timezone.now)
     # date_edited = models.DateTimeField(auto_now=True)
 
