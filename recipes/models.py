e823707e847c31a0e9c3d1bbab9eeee83352b065
Justin-Sawyer <justin.sawyer@icloud.com>
2021-08-04 08:13:15 +0000

    recipes model now includes all desired fields

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
 
 
 class Recipe(models.Model):
@@ -6,7 +8,16 @@ class Recipe(models.Model):
     intro = models.CharField(max_length=254)
     prep_time = models.DurationField()
     cook_time = models.DurationField()
+    # total_time = models.DurationField(default="", blank=True)
     servings = models.IntegerField()
+    directions = models.TextField(default="", blank=False)
+    image = models.ImageField(null=True, blank=True)
+    image_credit = models.CharField(max_length=254, default="", blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    date_posted = models.DateTimeField(null=True,
+                                       blank=True,
+                                       default=timezone.now)
+    date_edited = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -18,7 +29,8 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=150)
     preparation = models.CharField(max_length=150, default="", blank=True)
 
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                               related_name='ingredients')
 
     def __str(self):
         return self.name
