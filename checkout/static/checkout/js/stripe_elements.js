be0225a15ba3c3485e5de0c44b68460cbeda790d
Justin-Sawyer <justin.sawyer@icloud.com>
2021-08-20 09:20:50 +0000

    reset submit form checkout

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -108,7 +108,7 @@ form.addEventListener('submit', function(ev) {
         } else {
             // The payment has been processed
             if (result.paymentIntent.status === 'succeeded') {
-                //form.submit();
+                form.submit();
             }
         }
     });  
